d62f955e0c5941c47b4ff5c957cb212cfcdb6349
Aaron P. Rykhus <aprykhus@hotmail.com>
2020-01-16 18:41:16 -0600

    update to run every 5 seconds

--- grabsong.py
+++ grabsong.py
@@ -1,3 +1,10 @@
 from songengine import get_song
-currentsong = get_song('https://www.radio.com/wogl-vinyltap/listen')
-print(currentsong)
+import sched, time
+s = sched.scheduler(time.time, time.sleep)
+def run_script(sc): 
+    currentsong = get_song('https://www.radio.com/wogl-vinyltap/listen')
+    print(currentsong)
+    s.enter(5, 1, run_script, (sc,))
+
+s.enter(5, 1, run_script, (s,))
+s.run()
