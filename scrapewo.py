ef3df2003bd2a8199bbf29c0ae78e6017d65cbfc
Aaron P. Rykhus <aprykhus@hotmail.com>
2020-01-17 22:43:40 -0600

    Add line to suppress non-fatal console messages

--- scrapewo.py
+++ scrapewo.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
-import time
+# import time
 path = r'D:\\Documents\\code\\Python\\songscraper\\Chromedriver\\chromedriver.exe'
-driver = webdriver.Chrome(executable_path = path)
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument("--headless")
+chromeOptions.add_argument("--log-level=3")  # suppress console messages, restrict to fatal
+
+driver = webdriver.Chrome(options=chromeOptions, executable_path = path)
 driver.get('https://v7player.wostreaming.net/5792')
+driver
 driver.implicitly_wait(5)
 artist = driver.find_element_by_class_name('v7-cover-list__details')
 song = driver.find_element_by_class_name('v7-cover-list__title')
