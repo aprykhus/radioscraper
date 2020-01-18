ff34362cd03df49ed2a84d4bacee799667840aa0
Aaron P. Rykhus <aprykhus@hotmail.com>
2020-01-17 18:47:26 -0600

    update code to scrape station

--- scrapewo.py
+++ scrapewo.py
@@ -1,13 +1,21 @@
 from selenium import webdriver
+import time
 path = r'D:\\Documents\\code\\Python\\songscraper\\Chromedriver\\chromedriver.exe'
 driver = webdriver.Chrome(executable_path = path)
-driver.get('http://example.webscraping.com/places/default/search')
-driver.find_element_by_id('search_term').send_keys('.')
-js = "document.getElementById('page_size').options[1].text = '100';"
-driver.execute_script(js)
-driver.find_element_by_id('search').click()
-driver.implicitly_wait(45)
-links = driver.find_elements_by_css_selector('#results a')
-countries = [link.text for link in links]
-print(countries)
-driver.close()
+driver.get('https://v7player.wostreaming.net/5792')
+driver.implicitly_wait(5)
+artist = driver.find_element_by_class_name('v7-cover-list__details')
+song = driver.find_element_by_class_name('v7-cover-list__title')
+# artist = driver.find_element_by_class_name('v7-player__artist')
+# song = driver.find_element_by_class_name('v7-player__song-name')
+print(artist.text + " - " + song.text)
+driver.quit()
+
+#driver.find_element_by_id('search_term').send_keys('.')
+# js = "document.getElementById('page_size').options[1].text = '100';"
+# driver.execute_script(js)
+# driver.find_element_by_id('search').click()
+# links = driver.find_elements_by_css_selector('#results a')
+# countries = [link.text for link in links]
+# print(countries)
+# driver.close()
