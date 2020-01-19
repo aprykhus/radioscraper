ace017191ea7a2f517fb670e3c7d1710c0300c74
Aaron P. Rykhus <aprykhus@hotmail.com>
2020-01-18 18:17:50 -0600

    add Go button

--- songui.py
+++ songui.py
@@ -1,5 +1,12 @@
+# Create main window
 import tkinter as tk
 window = tk.Tk()
 window.minsize(400,600)
 window.geometry("700x700")
+
+
+# Button
+btnGo = tk.Button(window, text = "Go")
+btnGo.place(x = 600, y = 50)
+
 window.mainloop()
