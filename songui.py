618c7b8043ef9c8564ea85918c6b835e264a9277
Aaron P. Rykhus <aprykhus@hotmail.com>
2020-01-18 18:36:48 -0600

    add labels

--- songui.py
+++ songui.py
@@ -9,4 +9,16 @@ window.geometry("700x700")
 btnGo = tk.Button(window, text = "Go")
 btnGo.place(x = 600, y = 50)
 
+# Labels
+lblURL = tk.Label(window, text = "URL")
+lblURL.place(x = 50, y = 50)
+
+lblInterval = tk.Label(window, text = "Interval")
+lblInterval.place(x = 50, y = 100)
+
+lblTimeUnit = tk.Label(window, text = "seconds")
+lblInterval.place(x = 600, y = 100)
+
+# Entry
+
 window.mainloop()
