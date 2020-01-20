e8f5f9d288ef5274584931221c4d8bb28b7767fb
Aaron P. Rykhus <aprykhus@hotmail.com>
2020-01-19 21:31:21 -0600

    convert to class

--- scrapewo.py
+++ scrapewo.py
@@ -1,16 +1,21 @@
-if __name__ == "__main__":
-    from selenium import webdriver
-    # import time
-    path = r'D:\\Documents\\code\\Python\\songscraper\\Chromedriver\\chromedriver.exe'
+class Song:
+    def __init__(self):
+        self.artist = ""
+        self.title = ""
 
-    chromeOptions = webdriver.ChromeOptions()
-    chromeOptions.add_argument("--headless")
-    chromeOptions.add_argument("--log-level=3")  # suppress console messages, restrict to fatal
+    def grabSong(self, url):
+        from selenium import webdriver
+        # import time
+        path = r'D:\\Documents\\code\\Python\\songscraper\\Chromedriver\\chromedriver.exe'
 
-    driver = webdriver.Chrome(options=chromeOptions, executable_path = path)
-    driver.get('https://v7player.wostreaming.net/5792')
-    driver.implicitly_wait(3)
-    artist = driver.find_element_by_class_name('v7-cover-list__details')
-    song = driver.find_element_by_class_name('v7-cover-list__title')
-    print(artist.text + " - " + song.text)
-    driver.quit()
+        chromeOptions = webdriver.ChromeOptions()
+        chromeOptions.add_argument("--headless")
+        chromeOptions.add_argument("--log-level=3")  # suppress console messages, restrict to fatal
+
+        driver = webdriver.Chrome(options=chromeOptions, executable_path = path)
+        driver.get(url)
+        driver.implicitly_wait(3)
+        self.artist = driver.find_element_by_class_name('v7-cover-list__details')
+        self.title = driver.find_element_by_class_name('v7-cover-list__title')
+        print(self.artist.text + " - " + self.title.text)
+        driver.quit()
