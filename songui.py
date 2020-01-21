622b1663ec66921abbd64c65254e633119fec321
Aaron P. Rykhus <aprykhus@hotmail.com>
2020-01-20 20:49:08 -0600

    add code to scrape and populate listbox

--- songui.py
+++ songui.py
@@ -8,10 +8,15 @@ window.geometry("700x700")
 
 # Instantiate Song class
 objSong = scrapewo.Song()
+url = 'https://v7player.wostreaming.net/5792'
 
+def songCallback():
+    objSong.grabSong(entURL.get())
+    lbx.insert('end', objSong.artist + " - " + objSong.title)
+    lblState.config(bg = 'green')
 
 # Button
-btnGo = tk.Button(window, text = "Go")
+btnGo = tk.Button(window, text = "Go", command = songCallback)
 btnGo.place(x = 360, y = 50)
 
 # Labels
@@ -30,6 +35,7 @@ lblState.place(x = 360, y = 100, width = 25)
 # Entry
 entURL = tk.Entry(window)
 entURL.place(x = 100, y = 50, width = 250)
+entURL.insert(0, url)
 
 entInterval = tk.Entry(window)
 entInterval.place(x = 100, y = 100, width = 50)
@@ -37,9 +43,9 @@ entInterval.place(x = 100, y = 100, width = 50)
 #Listbox
 lbx = tk.Listbox(window)
 lbx.place(x = 50, y = 200, width = 500, height = 400)
-lbx.insert(1, "Kenny Wayne Shepard - Blue on Black")
-lbx.insert(2, "The Firm - Radioactive")
-lbx.insert(3, "Alan Parsons Project - Psychobabble")
-lbx.insert(4, "ZZ Top - Gimme All Your Lovin")
+# lbx.insert(1, "Kenny Wayne Shepard - Blue on Black")
+# lbx.insert(2, "The Firm - Radioactive")
+# lbx.insert(3, "Alan Parsons Project - Psychobabble")
+# lbx.insert(4, "ZZ Top - Gimme All Your Lovin")
 
 window.mainloop()
